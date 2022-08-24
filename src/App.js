28fe828160c1bd3d505bb791455c72b61667da5d
identityapproved <identityapproved@gmail.com>
2022-08-24 16:08:53 +0300

    first commit (create-react-app)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type='button'>
+        Learn React
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
